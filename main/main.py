54ae42ccb98dc84a5078d694dad1005fb0dcb510
Rahul Murugesan <rahulcode97@gmail.com>
2024-09-03 17:46:00 +0530

    dataset source link

---
+++ main/main.py
@@ -0,0 +1,43 @@
+import pickle
+import tensorflow as tf
+import numpy as np
+import time
+
+# load tokenizer word index
+with open('Culinary-Companion-word_index.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+# load trained rnn model
+recommendation_model = tf.keras.models.load_model('Culinary-Companion-V0.1.keras')
+
+def get_recipe_recommendation(ingredients: str) -> dict[str, str]:
+    max_seq_len = 232
+    for _ in range(max_seq_len):
+        token_list = tokenizer.texts_to_sequences([ingredients])[0]
+        token_list = tf.keras.utils.pad_sequences([token_list], maxlen=max_seq_len-1, padding = "pre")
+        predicted = recommendation_model.predict(token_list, verbose=0)
+        predicted = np.argmax(predicted, axis=-1)
+        
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                if(word == "~"):
+                    break
+                output_word = word
+                break
+        ingredients += " "+output_word
+
+    #ingredients = ingredients.encode("windows-1252").decode("utf-8")
+    outputlist = ingredients.split(" | ")
+    recipe = {}
+    recipe["NER"] = outputlist[0]
+    recipe["Recipe Title"] = outputlist[1]
+    recipe["Ingredients needed"] = outputlist[2]
+    recipe["Direction"] = outputlist[3]
+    return recipe
+
+print()
+print("Application output:")
+while(True):
+    ing = input("Enter comma seperated ingredients:")
+    print(get_recipe_recommendation(ing))
---
+++ src/Recommendation-system/dataset/README.md
@@ -0,0 +1,1 @@
+# Dataset availabel at [RecipeNLG](https://recipenlg.cs.put.poznan.pl/)
